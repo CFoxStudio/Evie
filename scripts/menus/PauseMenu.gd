b386ccbe41c8e446bce3546ed70dcb36b196e5a6
AndusDEV <30293380+AndusDEV@users.noreply.github.com>
2024-10-30 22:36:04 -1000

    Added clock to pause menu

--- scripts/menus/PauseMenu.gd
+++ scripts/menus/PauseMenu.gd
@@ -4,6 +4,10 @@ func _ready():
 	process_mode = ProcessMode.PROCESS_MODE_ALWAYS
 	$Center/Buttons/ResumeButton.grab_focus()
 
+func _process(delta):
+	var time = Time.get_time_dict_from_system()
+	$TimeLabel.text = "%02d:%02d:%02d" % [time.hour, time.minute, time.second]
+
 func _on_resume_button_pressed():
 	PauseManager.toggle_pause()
 
